229395583d5f33487de06603045d446edeb8ba95
Joker Yough <pw1316@outlook.com>
2018-10-21 20:40:53 +0800

    p9 trophy gen->py3;exclude dirs

--- code/get-trophy-p9/TrophyGenFromP9.py
+++ code/get-trophy-p9/TrophyGenFromP9.py
@@ -1,6 +1,6 @@
 #! usr/bin/python
-#coding=utf-8
-'''
+# coding=utf-8
+"""
 # <table cellpadding="12" cellspacing="0" border="0" width="100%" class="list">
 #   <tr><td colspan="4">
 #     <img src="???" width="100" height="55" class="imgbgnb l">
@@ -30,161 +30,166 @@
 #   <td width="12%" class="twoge t? h-p">???<em>珍贵度</em></td>
 #   </tr>
 # </table>
-'''
+"""
 import re
 import sys
-import urllib
+import urllib.request
 
-def decorate_input_url(strin):
-    '''Change gameId to actual URL
+
+def decorate_input_url(input_url):
+    """Change gameId to actual URL
 
     Args:
-        strin (str): The gameId we want to fetch
+        input_url (str): The gameId we want to fetch
 
     Returns:
         str: The actual URL of the game in P9
-    '''
+    """
 
-    assert isinstance(strin, str)
-    return 'http://psnine.com/psngame/' + strin
+    assert isinstance(input_url, str)
+    return "http://psnine.com/psngame/" + input_url
+
 
 def decorate_output_file(output_file_name, title, table_res):
-    '''Change gameId to actual URL
+    """Change gameId to actual URL
 
     Args:
         output_file_name (str): The file name to write
-        title (str): The title of the HTML doc
-        table_res (str): The trophy table
+        title (bytes): The title of the HTML doc
+        table_res (bytes): The trophy table
 
     Returns:
         str: The actual URL of the game in P9
-    '''
+    """
 
     assert isinstance(output_file_name, str)
-    assert isinstance(title, str)
-    assert isinstance(table_res, str)
-    output_file = open(output_file_name.decode('UTF-8'), 'wb')
-    output_file.write('<!DOCTYPE html>\n')
-    output_file.write('<html>\n')
-    output_file.write('  <head>\n')
-    output_file.write('    <title>' + title + '</title>\n')
-    output_file.write('    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">\n')
-    output_file.write('    <meta name="viewport" content="width=device-width">\n')
-    output_file.write('    <link rel="stylesheet" href="/stylesheets/p9/p9base.css">\n')
-    output_file.write('    <link rel="stylesheet" href="/stylesheets/p9/p9layer.css" ')
-    output_file.write('id="layui_layer_p9layercss">\n')
-    output_file.write('    <link rel="stylesheet" href="/stylesheets/pw-custom.css">\n')
-    output_file.write('    <link rel="Shortcut Icon" href="/src/keyaki.png">\n')
-    output_file.write('  </head>\n')
-    output_file.write('  <body style="background-color:#4E5566">\n')
-    output_file.write('    <div class="box main inner mt40">\n')
+    assert isinstance(title, bytes)
+    assert isinstance(table_res, bytes)
+    output_file = open(output_file_name, "wb")
+    output_file.write(b"<!DOCTYPE html>\n")
+    output_file.write(b"<html>\n")
+    output_file.write(b"  <head>\n")
+    output_file.write(b"    <title>" + title + b"</title>\n")
+    output_file.write(b"    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n")
+    output_file.write(b"    <meta name=\"viewport\" content=\"width=device-width\">\n")
+    output_file.write(b"    <link rel=\"stylesheet\" href=\"/stylesheets/p9/p9base.css\">\n")
+    output_file.write(b"    <link rel=\"stylesheet\" href=\"/stylesheets/p9/p9layer.css\" ")
+    output_file.write(b"id=\"layui_layer_p9layercss\">\n")
+    output_file.write(b"    <link rel=\"stylesheet\" href=\"/stylesheets/pw-custom.css\">\n")
+    output_file.write(b"    <link rel=\"Shortcut Icon\" href=\"/src/keyaki.png\">\n")
+    output_file.write(b"  </head>\n")
+    output_file.write(b"  <body style=\"background-color:#4E5566\">\n")
+    output_file.write(b"    <div class=\"box main inner mt40\">\n")
     output_file.write(table_res)
-    output_file.write('    </div>\n')
-    output_file.write('  </body>\n')
-    output_file.write('</html>\n')
+    output_file.write(b"    </div>\n")
+    output_file.write(b"  </body>\n")
+    output_file.write(b"</html>\n")
     output_file.close()
 
+
 def delete_white_space(doc):
-    '''Delete white spaces between tags in a HTML doc
+    """Delete white spaces between tags in a HTML doc
 
     Args:
-        doc (str): The doc to be filtered
+        doc (bytes): The doc to be filtered
 
     Returns:
         bytes: The doc without white spaces
-    '''
+    """
 
-    assert isinstance(doc, str)
-    return re.sub(r'\r?\n\s*|\t*', r'', doc)
+    assert isinstance(doc, bytes)
+    return re.sub(br"\r?\n\s*|\t*", br"", doc)
+
 
 def table_filter(table):
-    '''Remove unnecessary infomation in a trophy table
+    """Remove unnecessary information in a trophy table
 
     Args:
-        table (str): The HTML table to be filtered
+        table (bytes): The HTML table to be filtered
 
     Returns:
-        str: The table with only what we need
-    '''
+        bytes: The table with only what we need
+    """
 
-    assert isinstance(table, str)
-    match_res = re.search(r'(<table.*?>)(.*?)(</table>)', table)
-    assert match_res != None
+    assert isinstance(table, bytes)
+    match_res = re.search(br"(<table.*?>)(.*?)(</table>)", table)
+    assert match_res is not None
     table_head = match_res.group(1)
     table = match_res.group(2)
     table_tail = match_res.group(3)
-    table_res = '      ' + table_head + '\n'
-    table_res = table_res + '      <tr><td><a href="/index.html">Back</a></td></tr>\n'
+    table_res = b"      " + table_head + b"\n"
+    table_res = table_res + b"      <tr><td><a href=\"/index.html\">Back</a></td></tr>\n"
 
-    match_res = re.search(r'<tr.*?>.*?</tr>', table)
+    match_res = re.search(br"<tr.*?>.*?</tr>", table)
     if match_res:
         tag_tr = match_res.group(0)
         table = table[:match_res.start()] + table[match_res.end():]
-        tag_tr = re.sub(r'<img.*?><div.*?>', r'', tag_tr, 1)
-        tag_tr = re.sub(r'</div>', r'', tag_tr, 1)
-        table_res = table_res + '      ' + tag_tr + '\n'
+        tag_tr = re.sub(br"<img.*?><div.*?>", br"", tag_tr, 1)
+        tag_tr = re.sub(br"</div>", br"", tag_tr, 1)
+        table_res = table_res + b"      " + tag_tr + b"\n"
 
     while True:
-        match_res = re.search(r'<tr.*?>.*?</tr>', table)
-        if match_res:
-            tag_tr = match_res.group(0)
-            table = table[:match_res.start()] + table[match_res.end():]
-            # Delete <td> with image
-            pattern = r'<td class="t\d" width="\d*?" valign="top" align="center">'
-            pattern = pattern + r'<a href=".*?"><img src=".*?" width="\d*?" '
-            pattern = pattern + r'height="\d*?" class="imgbg" /></a></td>'
-            repl = r''
-            tag_tr = re.sub(pattern, repl, tag_tr, 1)
-            # Change <em class="h-p r"> to <em class="h-p">
-            pattern = r'<em class="h-p r">'
-            repl = r'<em class="h-p">'
-            tag_tr = re.sub(pattern, repl, tag_tr, 1)
-            # Change <a> to <span>
-            pattern = r'<a.*?(class[ ]*?=[ ]*?\S+)>(.*?)</a>'
-            repl = r'<span \1>\2</span>'
-            tag_tr = re.sub(pattern, repl, tag_tr, 1)
-            # Delete translation and tips <em>
-            pattern = r'<em>.*?</em>'
-            repl = r''
-            tag_tr = re.sub(pattern, repl, tag_tr, 1)
-            pattern = r'<em class="alert-success pd5"><b>\d*?</b> Tips</em>'
-            repl = r''
-            tag_tr = re.sub(pattern, repl, tag_tr, 1)
-            # Delete rare <td>
-            pattern = r'<td width="12%" class="twoge t. h-p">.*?</td>'
-            repl = r''
-            tag_tr = re.sub(pattern, repl, tag_tr, 1)
-            table_res = table_res + '      ' + tag_tr + '\n'
-        else:
+        match_res = re.search(br"<tr.*?>.*?</tr>", table)
+        if not match_res:
             break
+        tag_tr = match_res.group(0)
+        table = table[:match_res.start()] + table[match_res.end():]
+        # Delete <td> with image
+        pattern = br"<td class=\"t\d\" width=\"\d*?\" valign=\"top\" align=\"center\">"
+        pattern = pattern + br"<a href=\".*?\"><img src=\".*?\" width=\"\d*?\" "
+        pattern = pattern + br"height=\"\d*?\" class=\"imgbg\" /></a></td>"
+        repl = b""
+        tag_tr = re.sub(pattern, repl, tag_tr, 1)
+        # Change <em class="h-p r"> to <em class="h-p">
+        pattern = b"<em class=\"h-p r\">"
+        repl = b"<em class=\"h-p\">"
+        tag_tr = re.sub(pattern, repl, tag_tr, 1)
+        # Change <a> to <span>
+        pattern = br"<a.*?(class[ ]*?=[ ]*?\S+)>(.*?)</a>"
+        repl = br"<span \1>\2</span>"
+        tag_tr = re.sub(pattern, repl, tag_tr, 1)
+        # Delete translation and tips <em>
+        pattern = br"<em>.*?</em>"
+        repl = br""
+        tag_tr = re.sub(pattern, repl, tag_tr, 1)
+        pattern = br"<em class=\"alert-success pd5\"><b>\d*?</b> Tips</em>"
+        repl = br""
+        tag_tr = re.sub(pattern, repl, tag_tr, 1)
+        # Delete rare <td>
+        pattern = br"<td width=\"12%\" class=\"twoge t. h-p\">.*?</td>"
+        repl = br""
+        tag_tr = re.sub(pattern, repl, tag_tr, 1)
+        table_res = table_res + b"      " + tag_tr + b"\n"
 
-    table_res = table_res + '      ' + table_tail + '\n'
+    table_res = table_res + b"      " + table_tail + b"\n"
     return table_res
 
+
 def check_input():
-    '''Check input arguments if they are Valid
+    """Check input arguments if they are Valid
 
     Returns:
         list: The filtered argument list
         None: if the arguments are invalid
-    '''
+    """
 
     args = sys.argv
     if len(args) < 2:
-        print 'usage: %s <game no.> [<type1 type2 ...>]' % args[0]
-        print '<game no.>  The game ID in P9'
-        print '<type1 ...> Filter game types, default is "PS4"'
+        print("usage: %s <game no.> [<type1 type2 ...>]" % args[0])
+        print("<game no.>  The game ID in P9")
+        print("<type1 ...> Filter game types, default is \"PS4\"")
         return None
     if len(args) == 2:
-        args.append('PS4')
+        args.append("PS4")
     return args
 
+
 def main():
-    '''Main function
+    """Main function
 
     Returns:
         bool: True if is done, False if error occurs
-    '''
+    """
 
     # Arguments
     args = check_input()
@@ -193,65 +198,68 @@ def main():
 
     # Get Document
     try:
-        req = urllib.urlopen(decorate_input_url(args[1]))
+        req = urllib.request.urlopen(decorate_input_url(args[1]))
         doc = req.read()
         req.close()
     except BaseException:
-        print args[1] + "...not found"
+        print(args[1] + "...url open failed")
         return False
     doc = delete_white_space(doc)
+    assert isinstance(doc, bytes)
 
     # Get types block
-    match_res = re.search(r'<h1>(.*?)</h1>', doc)
+    match_res = re.search(br"<h1>(.*?)</h1>", doc)
     if match_res:
         type_string = match_res.group(1)
     else:
-        type_string = ''
+        type_string = ""
     remote_type_list = []
     # Get each type
     while True:
-        match_res = re.search(r'<span.*?>(.*?)</span>', type_string, re.S)
+        match_res = re.search(br"<span.*?>(.*?)</span>", type_string, re.S)
         if match_res:
             remote_type_list.append(match_res.group(1))
             type_string = type_string[:match_res.start()] + type_string[match_res.end():]
         else:
             break
-    if remote_type_list == []:
-        print args[1] + '...not found'
+    if not remote_type_list:
+        print(args[1] + "...no remote types")
         return False
     flag = False
     for i in args[2:]:
-        if i in remote_type_list:
+        if i.encode("utf-8") in remote_type_list:
             flag = True
             break
     if not flag:
-        print args[1] + '...not the wanted type'
+        print(args[1] + "...no type matches")
         return False
 
     # Process tables
     first_turn = True
-    table_res = ''
+    name = b""
+    table_res = b""
     while True:
-        match_res = re.search(r'<table.*?>.*?</table>', doc)
-        if match_res:
-            table = match_res.group(0)
-            doc = doc[:match_res.start()] + doc[match_res.end():]
-            if first_turn:
-                first_turn = False
-                name = re.search(r'<tr.*?>.*?<p>(.*?)</p>.*?</tr>', table).group(1)
-                name = re.sub(r'/|\\|\:|\*|\?|\"|<|>|\|', r'', name)
-            table = table_filter(table)
-            table_res = table_res + table
-        else:
+        match_res = re.search(br"<table.*?>.*?</table>", doc)
+        if not match_res:
             break
+        table = match_res.group(0)
+        doc = doc[:match_res.start()] + doc[match_res.end():]
+        # Get game name in first turn
+        if first_turn:
+            first_turn = False
+            name = re.search(br"<tr.*?>.*?<p>(.*?)</p>.*?</tr>", table).group(1)
+            name = re.sub(br"[/\\:*?\"<>|]", br"", name)
+        table = table_filter(table)
+        table_res = table_res + table
 
-    #write
-    output_name = args[1] + '-' + name
+    # write
+    output_name = args[1] + "-" + name.decode("UTF-8")
     for i in remote_type_list:
-        output_name = output_name + '-' + i
-    decorate_output_file(output_name + '.html', name, table_res)
-    print args[1] + '...done Types: ' + str(remote_type_list) + ' Name: ' + name.decode('UTF-8')
+        output_name = output_name + "-" + i.decode("UTF-8")
+    decorate_output_file(output_name + ".html", name, table_res)
+    print(args[1] + "...done Types: " + str(remote_type_list) + " Name: " + name.decode("UTF-8"))
     return True
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
